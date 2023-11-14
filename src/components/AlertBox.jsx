8b3bb534f2afe18b6746291288ceea73c5e3f17f
Alexey197 <margadonnn3@gmail.com>
2023-11-14 14:51:09 +0300

    intersection observer starting

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -1,12 +1,16 @@
 import { useRef } from "react"
 import styles from './AlertBox.module.css'
+import useIntersectionObserver from "../hooks/useIntersectionObserver"
 
 function AlertBox({ children }) {
   let el = useRef()
   let opened = true
+  let observer = useIntersectionObserver(el, '-200px')
+
+  console.log(observer);
 
   return <div 
-    className={`alert alert-success ${styles.alert} ${opened ? styles.alertShowed : ''}`} 
+    className={`alert alert-success ${styles.alert} ${observer?.isIntersecting ? styles.alertShowed : ''}`} 
     ref={el}>
     {children}
   </div>
---
+++ src/hooks/useIntersectionObserver.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useIntersectionObserver(elRef, rootMargin = '0px', onlyFirstShow = false) {
+
+  const [entry, setEntry ] = useState(null)
+
+  useEffect(() => {
+    if(!(elRef.current instanceof HTMLElement)) {
+      return
+    }
+    const observer = new IntersectionObserver(
+      ([ entry ]) => { setEntry(entry);},
+      { rootMargin }
+    )
+
+    observer.observe(elRef.current)
+    
+  }, [])
+
+  return entry
+}
