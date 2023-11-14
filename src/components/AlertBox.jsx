7250436f7b8968026ffb673f3781fa6ab72c0bab
Alexey197 <margadonnn3@gmail.com>
2023-11-14 11:22:48 +0300

    styles add

--- src/components/AlertBox.jsx
+++ src/components/AlertBox.jsx
@@ -1,9 +1,13 @@
 import { useRef } from "react"
+import styles from './AlertBox.module.css'
 
 function AlertBox({ children }) {
   let el = useRef()
+  let opened = true
 
-  return <div className="alert alert-success" ref={el}>
+  return <div 
+    className={`alert alert-success ${styles.alert} ${opened ? styles.alertShowed : ''}`} 
+    ref={el}>
     {children}
   </div>
 }
