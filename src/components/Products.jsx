3bc0541c3a35f5496de805ccdb920bb93edd653f
Alexey197 <margadonnn3@gmail.com>
2023-11-26 10:56:57 +0300

    useApi

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,19 +1,13 @@
-import { useContext, useEffect, useState } from "react"
-import ApiContext from "../context/api"
-function Product() {
-  let api = useContext(ApiContext)
-  const [ products, setProducts ] = useState([])
+import { useEffect, useState } from "react"
+import useApi from "../hooks/useApi"
 
-  useEffect(() => {
-    api.products.all().then(products => setProducts(products))
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
+function Product() {
+  let { data } = useApi('products.all')
 
   return <div>
     <h2>Catalod</h2>
     <div className="row">
-      {products.map(pr => <div key={pr.id} className="col col-2 m3">
+      {data instanceof Array && data.map(pr => <div key={pr.id} className="col col-2 m3">
         <h3>{ pr.title }</h3>
       </div>)}
     </div>
