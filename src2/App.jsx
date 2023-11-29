cfd7cae633cc78a0da7f057fdf78a72c7cb98afb
Алексей Даньев <aleksejdanev@MacBook-Pro-Aleksej-2.local>
2023-11-29 09:21:01 +0300

    router mbp

--- src2/App.jsx
+++ src2/App.jsx
@@ -1,42 +1,41 @@
-import { useRoutes, Link } from "react-router-dom"
-import routes from "./routes"
+import { useRoutes, Link } from "react-router-dom";
+import routes from "./routes";
 
 export default function App() {
-	const element = useRoutes(routes)
+  const element = useRoutes(routes);
 
-	return <div>
-		<header>
-			<div className="container">
-				Logo
-				Cart
-			</div>
-		</header>
-		<div>
-			<div className="container">
-				<div className="row">
-					<div className="col col-3">
-						<ul className="list-group">
-							<li className="list-group-item">
-								<Link to="/">Catalog</Link>
-							</li>
-							<li className="list-group-item">
-								<Link to="/cart">Cart</Link>
-							</li>
-						</ul>
-					</div>
-					<div className="col col-9">
-
-					</div>
-				</div>
-				{ element }
-			</div>
-		</div>
-		<footer>
-			<div className="container">
-				footer
-				Cart
-			</div>
-		</footer>
-	</div>
-	
+  return (
+    <div>
+      <header>
+        <div className="container">Logo Cart</div>
+      </header>
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col col-3">
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link to="/">Catalog</Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/cart">Cart</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col col-9"></div>
+          </div>
+          {element ?? 
+						<div>
+							<h3>
+								Page not found
+							</h3>
+						</div>
+					}
+        </div>
+      </div>
+      <footer>
+        <div className="container">footer Cart</div>
+      </footer>
+    </div>
+  );
 }
